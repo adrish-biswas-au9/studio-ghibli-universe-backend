55c4e82308666232959b00adac530e1e54ab0776
Adrish <adrishlinkin@gmail.com>
2021-04-01 01:10:46 +0530

    seventy-third commit

--- modal/characterModal.js
+++ modal/characterModal.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 const characterSchema = new mongoose.Schema(
     {
-        "id": String,
-        "merch_link": String,
+        "id": Number,
         "name": String,
+        "gender": String,
+        "age": Number,
+        "eye_color": String,
+        "hair_color": String,
+        "films": Array,
+        "film_image_url": String,
+        "species": String,
         "image_url": String
-    }
+      }
 )
 //mongoose.model('collection','schema)
 mongoose.model('character', characterSchema);
